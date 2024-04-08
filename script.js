399898298a54c5a89a44a65566add3d4a1a37bdc
Jon Merilä <jon.merila97@hotmail.com>
2024-04-08 12:09:22 +0200

    Added pokemon stats and type in DOM

--- script.js
+++ script.js
@@ -22,18 +22,45 @@ class Pokemon {
 
 const loadPokemons = () => {
   console.log(selectedPokemons);
-  let pokeCard = document.createElement("div");
-  let pokeSprite = document.createElement("div");
-  let img = document.createElement("img");
-  let pokeTitle = document.createElement("h3");
-  let pokestats = document.createElement("div");
-  let statpara = (document.createElement("p").innerText = "Stats: ");
-  let statList = document.createElement("ul");
-  /*Object.keys(list) => ((key) => {
-    let li = document.createElement("li");
-    li.innerHTML = `${key}: ${list[key]}`;
-    statList.append(li);
-  }) */
+  selectedPokemons.forEach((pokemon) => {
+    console.log(pokemon);
+    let pokeCard = document.createElement("div");
+
+    let pokeSprite = document.createElement("div");
+    let img = document.createElement("img");
+    img.src = pokemon.avatar;
+
+    let pokeTitle = document.createElement("h3");
+    pokeTitle.innerText = pokemon.name;
+    let pokeHp = document.createElement("strong");
+    let currentHp = pokemon.stats.hp;
+    pokeHp.innerHTML = `HP: ${currentHp}/${pokemon.stats.hp}`;
+
+    let pokestats = document.createElement("div");
+    let statpara = (document.createElement("p").innerText = "Stats: ");
+    let statList = document.createElement("ul");
+
+    let poketype = document.createElement("p");
+    let allTypes = pokemon.typing
+      .map((typeObj) => typeObj.type.name)
+      .join(", ");
+    poketype.innerHTML = `Type(s): ${allTypes}`;
+
+    Object.keys(pokemon.stats).forEach((key) => {
+      let li = document.createElement("li");
+      if (key === "hp") {
+        li.innerHTML = `base ${key}: ${pokemon.stats[key]}`;
+        statList.append(li);
+      } else {
+        li.innerHTML = `${key}: ${pokemon.stats[key]}`;
+        statList.append(li);
+      }
+    });
+    pokeSprite.append(img);
+    pokestats.append(statpara, statList);
+    pokeCard.append(pokeSprite, pokeTitle, pokeHp, poketype, pokestats);
+    pokemonContainer.append(pokeCard);
+  });
 };
 
 const getPokemonData = async (url) => {
